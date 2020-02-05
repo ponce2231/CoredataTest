0271239246f4840abec96ed646d9c07096c1d043
Christopher Ponce Mendez <christopherponce@hotmail.com>
2020-02-05 10:41:18 -0400

    First Commit

---
+++ CoredataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoredataTest
+//
+//  Created by Christopher Ponce Mendez on 2/5/20.
+//  Copyright © 2020 Christopher Ponce Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

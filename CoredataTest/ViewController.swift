11c633ae00fab998b6cdd1b0fffc886898831e0a
Christopher Ponce Mendez <christopherponce@hotmail.com>
2020-02-05 16:40:49 -0400

    created coredata model entity Person with the attribute name, called nsmanaged object to pass the entity and its attibute, created a save function to save in coredata the value

--- CoredataTest/ViewController.swift
+++ CoredataTest/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: variables
-    var names: [String] = []
+//    var names: [String] = []
+    var people: [NSManagedObject] = []
     
     
     override func viewDidLoad() {
@@ -32,7 +34,7 @@ class ViewController: UIViewController {
             guard let textfield = alert.textFields?.first, let nameToSave = textfield.text else{
                 return
             }
-            self.names.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
@@ -43,18 +45,40 @@ class ViewController: UIViewController {
         
         present(alert, animated: true, completion: nil)
     }
+//    MARK: STORE TO COREDATA
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            return
+        }
+        //MARK: STEP 1
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+//        MARK: STEP 2
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+//        MARK: STEP 3
+        person.setValue(name, forKeyPath: "name")
+        
+//        MARK: STEP 4
+        do{
+            try managedContext.save()
+        }catch let error as NSError{
+            print("Could not save \(error), \(error.userInfo) ")
+        }
+    }
 }
 //MARK: DataSource Functions
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        cell.textLabel?.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
         
         return cell
     }

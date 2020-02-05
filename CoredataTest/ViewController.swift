5a3804aa74b5aef2ac7780036d0396a7a64a59e7
Christopher Ponce Mendez <christopherponce@hotmail.com>
2020-02-05 12:07:58 -0400

    added functionality to the addName ibaction and setup and alertcontroller to save and cancel actions and reload table when finished

--- CoredataTest/ViewController.swift
+++ CoredataTest/ViewController.swift
@@ -24,12 +24,27 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
-
+//    MARK: Actions
     @IBAction func addName(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New", message: "Add new name", preferredStyle: .alert)
         
+        let saveAction = UIAlertAction(title: "save", style: .default) { [unowned self] action in
+            guard let textfield = alert.textFields?.first, let nameToSave = textfield.text else{
+                return
+            }
+            self.names.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
-
+//MARK: DataSource Functions
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return names.count

978892f5b40c9d047c925a496ff4cf11d893f007
Christopher Ponce Mendez <christopherponce@hotmail.com>
2020-02-05 11:43:04 -0400

    added an ibaction for the bar button and a tableview outlet, set the navbar title and used the register method

--- CoredataTest/ViewController.swift
+++ CoredataTest/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: variables
+    var name: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "The List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
 
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        
+    }
 }
 
